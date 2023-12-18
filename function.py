4ac35c000405d6f6f0c25028596b2514cae3622e
Amirtha-2805 <amirthanatarajan14@gmail.com>
2023-12-17 23:53:34 -0800

    function.py added

---
+++ function.py
@@ -0,0 +1,16 @@
+
+def color():
+    print("Blue")
+    print("White")
+color()   
+def add(a,b):
+    print(a+b)
+add(4,3)
+
+def multiply(x,y):
+    print(x*y)
+multiply(10,2)  
+
+def divide(a,b):
+    print(a/b)
+divide(144,12)    
